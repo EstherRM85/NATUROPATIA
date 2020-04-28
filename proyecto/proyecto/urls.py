3eeeb904789f8809740136b30767a4a3d62972da
Maelly <maellysilver@gmail.com>
2020-04-28 12:50:43 +0200

    Traduccion (es-en) Barra de navegacion

--- proyecto/proyecto/urls.py
+++ proyecto/proyecto/urls.py
@@ -17,10 +17,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),      
+]
+
+urlpatterns += i18n_patterns (
     path('home/',include(('app.urls','app'))),
-   
-] + static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+)+ static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
